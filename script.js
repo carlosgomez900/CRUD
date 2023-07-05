51a79de02c2506caf540023ae8169a4c4010b9b7
carlos <carlosgomezal02@outlook.com>
2023-07-04 21:55:59 -0600

    se esta estructurando js para terminar el proyecto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Validate form inputs before submiting data
 function validateForm(){
     var titulo = document.getElementById("titulo").value; 
     var consola = document.getElementById("consola").value; 
@@ -14,7 +15,7 @@ function validateForm(){
         return false
     }
     if(precio == ""){
-        alert("Elprecio es requerido");
+        alert("El precio es requerido");
         return false
     }
 
@@ -29,6 +30,7 @@ function validateForm(){
     }
 }
 
+// Fuction to show Data
 function showData(){
     var peopleList;
     if(localStorage.getItem("peopleList") == null){
@@ -42,6 +44,51 @@ function showData(){
 
     peopleList.forEach(function (element, index){
         html += "<tr>";
-        html += "<td>" +;
-    })
+        html += "<td>" + element.titulo + "</td>";
+        html += "<td>" + element.consola + "</td>";
+        html += "<td>" + element.precio + "</td>";
+        html += "<td>" + element.clasificacion + "</td>";
+        html +=
+        '<td><button onclick="deleData"('+ index +')" class="btn btn-danger">Delete</button><button onclick="updateData(' + index +')" class="btn btn-warning m-2">EDIT</button></td>';
+     html +="</tr>";       
+    });
+
+    document.querySelector("#crudTable tbody").innerHTML = html;
+}
+
+//Loads All datawhen document or page loaded
+ducument.onload = showData();
+
+//Fumction to add data
+
+function actualizar(){
+    //if form is validate
+    if(validateForm() == true){
+        var titulo =document.getElementById("titulo").value;
+        var consola =document.getElementById("consola").value;
+        var precio =document.getElementById("precio").value;
+        var clasificacion =document.getElementById("clasificacion").value;
+
+        var  peopleList;
+        if(localStorage.getItem("peopleList") == null){
+            peopleList = [];
+        } 
+        else {
+            peopleList = JSON.parse(localStorage.getItem("peopleList"));
+        }
+
+        peopleList.push({
+            titulo: titulo,
+            consola: consola,
+            precio: precio,
+            categoria: categoria,
+        });
+
+        localStorage.setItem("peopleList", JSON.stringify (peopleList));
+        showData();
+        document.getElementById("titulo").value = "";
+        document.getElementById("consola").value = "";
+        document.getElementById("precio").value = "";
+        document.getElementById("categoria").value = "";
+    }
 }
