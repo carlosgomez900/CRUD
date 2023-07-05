fe3e61b8f62f78318ab0416733871af33b818b8f
carlos <carlosgomezal02@outlook.com>
2023-07-05 03:08:15 -0600

    se anexa carpeta

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function validateForm(){
         alert("La clasificacion es requerido");
         return false
     }
+    return true;
 }
 
 // Fuction to show Data
@@ -49,20 +50,29 @@ function showData(){
         html += "<td>" + element.precio + "</td>";
         html += "<td>" + element.clasificacion + "</td>";
         html +=
-        '<td><button onclick="deleData"('+ index +')" class="btn btn-danger">Delete</button><button onclick="updateData(' + index +')" class="btn btn-warning m-2">EDIT</button></td>';
+
+        '<td><button onclick="deleteData('+ 
+        index +
+        ')" class="btn btn-danger">Delete</button><buttononclick = "updateData (' +
+        index +
+        ')" class="btn btn-warning m-2">Edit</button></td>';
+
      html +="</tr>";       
     });
 
-    document.querySelector("#crudTable tbody").innerHTML = html;
+    document.querySelector("#crudTable tbody").innerHTML = 
+    html;
 }
 
-//Loads All datawhen document or page loaded
-ducument.onload = showData();
+//Loads All data when document or page loaded
+document.onload = showData();
 
-//Fumction to add data
+//Function to add data
 
-function actualizar(){
+function AddData(){
+
     //if form is validate
+
     if(validateForm() == true){
         var titulo =document.getElementById("titulo").value;
         var consola =document.getElementById("consola").value;
@@ -70,25 +80,95 @@ function actualizar(){
         var clasificacion =document.getElementById("clasificacion").value;
 
         var  peopleList;
-        if(localStorage.getItem("peopleList") == null){
+        if(localStorage.getItem("peopleList") == null) {
             peopleList = [];
-        } 
-        else {
-            peopleList = JSON.parse(localStorage.getItem("peopleList"));
+        } else {
+            peopleList = JSON.parse(localStorage.getItem 
+            ("peopleList"));
         }
 
         peopleList.push({
             titulo: titulo,
             consola: consola,
             precio: precio,
-            categoria: categoria,
+            clasificacion: clasificacion,
         });
 
-        localStorage.setItem("peopleList", JSON.stringify (peopleList));
+        localStorage.setItem("peopleList", JSON.stringify 
+        (peopleList));
         showData();
         document.getElementById("titulo").value = "";
         document.getElementById("consola").value = "";
         document.getElementById("precio").value = "";
-        document.getElementById("categoria").value = "";
+        document.getElementById("clasificacion").value = "";
+    }
+}
+
+//function to delete Date form local storage
+
+function deleteData(index){
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    } else {
+        peopleList = JSON.parse(localStorage.getItem 
+        ("peopleList"));
+    }
+
+    peopleList.splice(index, 1);
+    localStorage.setItem("peopleLlist", JSON.stringify 
+    (peopleList));
+    showData();
+}
+
+//fuction to update/edit data in local storage
+
+function updateData(index){
+    //submit button 
+    document.getElementById("Submit").style.display = "none";
+    document.getElementById("update").style.display = "block";
+
+    var peopleList;
+    if (localStorage.getItem ("peopleList") == null) {
+        peopleList = [];
+
+    } else {
+        peopleList = JSON.parse(localStorage.getItem
+        ("peopleList"));
+    }
+
+    document.getElementById("titulo").value = peopleList[index].titulo;
+    document.getElementById("consola").value = peopleList[index].consola;
+    document.getElementById("precio").value = peopleList[index].precio;
+    document.getElementById("clasificacion").value = peopleList[index].clasificacion;
+
+    document.querySelector("#update").onclick = function(){
+        if(validateForm() == true){
+            peopleList[index].titulo = document.getElementById
+            ("titulo").value;
+            peopleList[index].consola = document.getElementById
+            ("consola").value;
+            peopleList[index].precio = document.getElementById
+            ("precio").value;
+            peopleList[index].clasificacion = document.getElementById
+            ("clasificacion").value;
+
+        localStorage.setItem("peopleList", JSON.stringify
+        (peopleList));
+
+        showData();
+
+        document.getElementById("titulo").value ="";
+        document.getElementById("consola").value ="";
+        document.getElementById("precio").value ="";
+        document.getElementById("clasificacion").value ="";
+
+        //update button
+
+        document.getElementById("Submit").style.display = 
+        "block";
+        document.getElementById("Update").style.display = 
+        "none";
+        }
     }
 }
